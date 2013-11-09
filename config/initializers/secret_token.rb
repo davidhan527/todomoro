d674382966a56d1fbcf30ff596dacf51c3b97940
davidhan527 <davidhan527@gmail.com>
2013-11-09 11:47:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todomoro::Application.config.secret_key_base = '5e7e717795474e13a01dc75f68235964954da8e9da4c8f945cc138818000b9b5f1514f607b30d93b9a4e566e04f56fc1acd19b643eff6b4413d5dd35f0e1d1bf'
