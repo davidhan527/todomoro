de64c0c92e1afedddf9570348e810306fbc97b49
davidhan527 <davidhan527@gmail.com>
2013-11-09 14:47:07 -0500

    adding omniauth-github sign in

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '4.0.0'
 # Use postgresql as the database for Active Record
 gem 'pg'
 
+gem 'pry'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
@@ -48,3 +50,4 @@ group :test do
 end
 
 gem 'omniauth-twitter'
+gem 'omniauth-github'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   # list other providers below here for omniauth authentication
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
